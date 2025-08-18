8d0b472a21ce25d0fd79de39274539ebc05a9a29
mustofa adny <adniadni04@gmail.com>
2025-08-18 15:49:08 +0700

    Props

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,16 @@ const name = 'Ucok Udin'
 const blogPost = "<p style='color: red'>Ini adalah blog post</p>"
 const idBlog = 1
 const blogClass = 'text-2xl flex flex-col'
+const submitButton = {
+  title: 'Submit',
+}
 </script>
 
 <template>
-  <MainButton />
+  <MainButton :title="`test button`" :number-button="1" />
+  <MainButton :title="`test button`" :number-button="2" />
+  <MainButton :title="submitButton.title" :number-button="3" />
+  <!--shorthand: v-bind="submitButton"-->
   <p>Hello, {{ name }}</p>
   <OptionsComponents />
   <CompositionComponents />
--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -17,17 +17,22 @@ const url = {
   attr: 'href',
   link: '/home',
 }
+const { title, numberButton } = defineProps({
+  title: String,
+  numberButton: Number,
+})
 </script>
 
 <template>
-  <button :disabled="buttonDisabled" v-bind="objectofAttrs" :id="`id-${objectofAttrs.id}`">
+  <!-- <button :disabled="buttonDisabled" v-bind="objectofAttrs" :id="`id-${objectofAttrs.id}`">
     {{ isLoading ? 'loading' : 'submit' }}
   </button>
   <a :[url.attr]="url.link">
     <button :disabled="buttonDisabled" v-bind="objectofAttrs" :id="`id-${objectofAttrs.id}`">
       {{ renderIsLoading() }}
     </button>
-  </a>
+  </a> -->
+  <button>{{ title }} {{ numberButton }}</button>
 </template>
 
 <style scoped></style>
