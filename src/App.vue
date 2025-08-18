d4e71f380c34414070581cd21f8f345386715dc4
mustofa adny <adniadni04@gmail.com>
2025-08-18 15:11:37 +0700

    Interpolations & Bindings

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 import MainButton from './components/MainButton.vue'
 
 const name = 'Ucok Udin'
+const blogPost = "<p style='color: red'>Ini adalah blog post</p>"
+const idBlog = 1
+const blogClass = 'text-2xl flex flex-col'
 </script>
 
 <template>
@@ -9,6 +12,10 @@ const name = 'Ucok Udin'
   <p>Hello, {{ name }}</p>
   <OptionsComponents />
   <CompositionComponents />
+
+  <div v-bind:id="idBlog" :class="blogClass">{{ blogPost }}</div>
+  <!--shorthand: :id="idBlog"-->
+  <div v-html="blogPost"></div>
 </template>
 
 <style scoped></style>
--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,7 +1,26 @@
-<script setup></script>
+<script setup>
+const objectofAttrs = {
+  id: 'product-1',
+  class: 'button',
+}
+const isLoading = true
+const buttonDisabled = isLoading
+const renderIsLoading = () => {
+  if (isLoading) {
+    return 'loading2'
+  } else {
+    return 'submit2'
+  }
+}
+</script>
 
 <template>
-  <button>Button</button>
+  <button :disabled="buttonDisabled" v-bind="objectofAttrs" :id="`id-${objectofAttrs.id}`">
+    {{ isLoading ? 'loading' : 'submit' }}
+  </button>
+  <button :disabled="buttonDisabled" v-bind="objectofAttrs" :id="`id-${objectofAttrs.id}`">
+    {{ renderIsLoading() }}
+  </button>
 </template>
 
 <style scoped></style>
